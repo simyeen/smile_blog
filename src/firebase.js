74f2504c05bfa8006783ac3d5ca93264c5cf2fc4
simyeen <simyeen@naver.com>
2021-11-04 21:07:51 +0900

    feat/module

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,21 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
+// import "firebaseInstance/compat/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBTge8cNNawjM2YBBR1UluWT_nUyYYEtAs",
-  authDomain: "blog-7e790.firebaseapp.com",
-  projectId: "blog-7e790",
-  storageBucket: "blog-7e790.appspot.com",
-  messagingSenderId: "280441648109",
-  appId: "1:280441648109:web:f8844f3923736a284dade4",
-  measurementId: "G-1HR609HB4D",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASURMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
 
-export default db;
+export const firebaseInstance = firebase;
+export const dbService = firebase.firestore();
+export const postRef = dbService.collection("post");
+
+// export const storageService = firebase.storage();
