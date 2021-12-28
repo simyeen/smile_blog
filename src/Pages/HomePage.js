58ea25eec8d5d6e1f049b6e52475056126bb1385
simyeen <simyeen@naver.com>
2021-12-28 21:59:02 +0900

    tesing api

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import styled from "styled-components";
 import HeaderContaner from "../Containers/common/HeaderContaner";
 import PostListContainer from "../Containers/post/PostListContainer";
 
 const HomePage = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+
+    script.type = "text/javascript";
+    script.src =
+      "//dapi.kakao.com/v2/maps/sdk.js?appkey=f5c21470a2a1fd83bbae45b685b323c7";
+    script.async = true;
+
+    document.body.appendChild(script);
+  }, []);
+
   return (
     <>
       <HeaderContaner />
-      <PostListContainer />
+      {/* <PostListContainer /> */}
+      <KakaoMap>안녕 나는 지도</KakaoMap>
     </>
   );
 };
 
 export default HomePage;
+
+const KakaoMap = styled.div``;
